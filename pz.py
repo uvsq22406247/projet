42ed1cd1fa93df788457978a397a78069f498004
Prénom NOM <prenom.nom@ens.uvsq.fr>
2025-03-05 13:09:13 +0100

    zack

--- pz.py
+++ pz.py
@@ -1,2 +1,19 @@
 print("hello")
+<<<<<<< HEAD
 print("roro")
+=======
+print("roro")
+=======
+print("hello")
+print("Rothen")
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 7cbb445 (rothren)
+test
+=======
+print("")
+>>>>>>> f5d2d1a (Zack)
+=======
+print("Z")
+>>>>>>> 7dad615 (zack)
+>>>>>>> 5c680f7 (zack)
