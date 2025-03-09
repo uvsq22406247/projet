729f77c7c5b1e6323441d210bfe2b1210faa138d
Zack <zackari.laoubi@ens.uvsq.fr>
2025-03-09 19:23:20 +0100

    Zackari

--- Puissance4.py
+++ Puissance4.py
@@ -1,1 +1,24 @@
 import tkinter as tk
+from tkinter import messagebox
+
+# Constantes
+WIDTH = 700
+HEIGHT = 600
+
+
+# Variables globales
+root = None
+
+def clear_window():
+    for widget in root.winfo_children():
+        widget.destroy()
+
+    
+def main():
+    global root
+    root = tk.Tk()
+    root.title("Puissance 4")
+    root.geometry(f"{WIDTH}x{HEIGHT}")
+    root.mainloop()
+    
+
