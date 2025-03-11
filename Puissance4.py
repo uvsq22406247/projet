28e3f9418d6088fcc46af7fd01fba1f995cb08d8
Zack <zackari.laoubi@ens.uvsq.fr>
2025-03-11 12:40:28 +0100

    Zack

--- Puissance4.py
+++ Puissance4.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox #Importe le module messagebox pour afficher des boîtes de dialogue.
 
 # Constantes
 WIDTH = 700
@@ -7,19 +7,19 @@ HEIGHT = 600
 
 
 # Variables globales
-root = None
+root = None # root est la fenêtre de base de l'application. Tous les autres éléments graphiques y sont attachés.
 
-def clear_window():
+def clear_window(): #Cette fonction est utilisée pour effacer tous les éléments graphiques de la fenêtre principale.
     for widget in root.winfo_children():
-        widget.destroy()
+        widget.destroy() #Cette méthode supprime un widget de la fenêtre. Lorsqu'elle est appelée, le widget est détruit et retiré de l'interface graphique.
 
     
 def main():
-    global root
-    root = tk.Tk()
-    root.title("Puissance 4")
+    global root 
+    root = tk.Tk() # Crée la fenêtre principale
+    root.title("Puissance 4") # Définit le titre de la fenêtre
     root.geometry(f"{WIDTH}x{HEIGHT}")
-    root.mainloop()
+    root.mainloop()# Lance la boucle principale de l'interface graphique
     
 print("")
 print('')
